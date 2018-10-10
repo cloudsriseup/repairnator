1d95d025d727bb79af22b6d551539ef3dae0183f
Benjamin Loriot <benjamin.loriot@etu.utc.fr>
2018-10-10 14:18:38 +0200

    vue + realtime update + notifications

--- website/repairnator-site/app/scripts/realtime_values.js
+++ website/repairnator-site/app/scripts/realtime_values.js
@@ -1,86 +1,119 @@
-$.get(getApiUri('/inspectors/'), function (datas) {
-  var htmlElement = $('#tablerealtime');
+/*Vue.component('inspector-line', {
+  props: ['inspector'],
+  template: '<tr><td>{{inspector.buildId}}</td></tr>'
+})
 
-  var fieldNames = [
-    {id:'buildFinishedDate', readable: 'Original date'},
-    {id: 'buildReproductionDate', readable: 'Date of the reproduction'},
-    {id: 'buildId', readable: 'Build ID'},
-    {id: 'repositoryName', readable: 'Github Repository'},
-    {id: 'status', readable: 'Status'},
-    {id: 'prNumber', readable: 'Pull Request ID'},
-    {id: 'travisURL', readable: 'URL of Travis build'},
-    {id: 'typeOfFailures', readable: 'Type of failures'},
-    {id: 'branchURL', readable: 'URL of the branch'}
-  ];
 
-  var headersDisplayed = false;
-  datas.forEach(function (data) {
-    var row = $('<tr></tr>');
-    htmlElement.append(row);
+var appInspectors = new Vue({
+  el: '#app-inspectors',
+  data: {
+    inspectors: [
+      { buildId: 0, text: 'Vegetables' },
+      { buildId: 1, text: 'Cheese' },
+      { buildId: 2, text: 'Whatever else humans are supposed to eat' }
+    ]
+  }
+})*/
 
-    if (!headersDisplayed) {
-      fieldNames.forEach(function (fieldName) {
-        var th = $('<th></th>');
-        th.text(fieldName.readable);
+let lastBuild;
 
-        row.append(th);
-      });
+const updateInspectors = function(){
+  apiGet('/inspectors/', function (datas) {
+    var htmlElement = $('#tablerealtime');
+    htmlElement.html("")
 
-      row = $('<tr></tr>');
-      htmlElement.append(row);
-      headersDisplayed = true;
+    var fieldNames = [
+      {id:'buildFinishedDate', readable: 'Original date'},
+      {id: 'buildReproductionDate', readable: 'Date of the reproduction'},
+      {id: 'buildId', readable: 'Build ID'},
+      {id: 'repositoryName', readable: 'Github Repository'},
+      {id: 'status', readable: 'Status'},
+      {id: 'prNumber', readable: 'Pull Request ID'},
+      {id: 'travisURL', readable: 'URL of Travis build'},
+      {id: 'typeOfFailures', readable: 'Type of failures'},
+      {id: 'branchURL', readable: 'URL of the branch'}
+    ];
+
+    var headersDisplayed = false;
+    // appInspectors.inspectors = datas;
+    if (lastBuild){
+      if (datas[0].buildId != lastBuild){
+        lastBuild = datas[0].buildId;
+        notify(`New build found ${datas[0].status}`)
+      }
+    } else {
+      lastBuild = datas[0].buildId;
     }
+    datas.forEach(function (data) {
+      var row = $('<tr></tr>');
+      htmlElement.append(row);
 
-    fieldNames.forEach(function (column) {
-      var fieldName = column.id;
-      var td = $('<td></td>');
+      if (!headersDisplayed) {
+        fieldNames.forEach(function (fieldName) {
+          var th = $('<th></th>');
+          th.text(fieldName.readable);
 
-      var dataValue = data[fieldName];
+          row.append(th);
+        });
 
-      if (fieldName == 'buildFinishedDate') {
-        dataValue = moment(dataValue).subtract(2, 'hours').fromNow();
+        row = $('<tr></tr>');
+        htmlElement.append(row);
+        headersDisplayed = true;
       }
 
-      if (fieldName == 'buildReproductionDate') {
-        dataValue = moment(dataValue).subtract(2, 'hours').fromNow();
-      }
+      fieldNames.forEach(function (column) {
+        var fieldName = column.id;
+        var td = $('<td></td>');
 
-      if (fieldName == 'status') {
-        if (data[fieldName] == 'PATCHED') {
-          row.addClass('success');
-        } else if (data[fieldName] == 'test failure' || data[fieldName] == 'test errors') {
-          row.addClass('warning');
-        }
-      }
+        var dataValue = data[fieldName];
 
-      if (fieldName == 'prNumber') {
-        if (dataValue != 0) {
-          dataValue = '<a href="https://github.com/'+data['repositoryName']+'/pull/'+data[fieldName]+'"><img src="images/github-logo.svg" style="width: 40px; height: 40px" alt="'+dataValue+'" /></a>';
-        } else {
-          dataValue = '';
-        }
-      }
-
-      if (fieldName == 'typeOfFailures' && dataValue != null) {
-        dataValue = dataValue.split(',').join(' ');
-      }
-
-      if (fieldName == 'travisURL') {
-        dataValue = '<a href="'+dataValue+'"><img src="images/travis-ci.png" style="width: 40px; height: 40px" alt="'+dataValue+'" /></a>';
-      }
-
-      if (fieldName == 'branchURL') {
-        if (dataValue != undefined && dataValue != null) {
-          dataValue = '<a href="'+dataValue+'"><img src="images/github-logo.svg" style="width: 40px; height: 40px" alt="Go to branch" /></a>';
-        } else {
-          dataValue = 'N/A';
+        if (fieldName == 'buildFinishedDate') {
+          dataValue = moment(dataValue).subtract(2, 'hours').fromNow();
         }
 
-      }
+        if (fieldName == 'buildReproductionDate') {
+          dataValue = moment(dataValue).subtract(2, 'hours').fromNow();
+        }
 
-      td.html(dataValue);
+        if (fieldName == 'status') {
+          if (data[fieldName] == 'PATCHED') {
+            row.addClass('success');
+          } else if (data[fieldName] == 'test failure' || data[fieldName] == 'test errors') {
+            row.addClass('warning');
+          }
+        }
 
-      row.append(td);
+        if (fieldName == 'prNumber') {
+          if (dataValue != 0) {
+            dataValue = '<a href="https://github.com/'+data['repositoryName']+'/pull/'+data[fieldName]+'"><img src="images/github-logo.svg" style="width: 40px; height: 40px" alt="'+dataValue+'" /></a>';
+          } else {
+            dataValue = '';
+          }
+        }
+
+        if (fieldName == 'typeOfFailures' && dataValue != null) {
+          dataValue = dataValue.split(',').join(' ');
+        }
+
+        if (fieldName == 'travisURL') {
+          dataValue = '<a href="'+dataValue+'"><img src="images/travis-ci.png" style="width: 40px; height: 40px" alt="'+dataValue+'" /></a>';
+        }
+
+        if (fieldName == 'branchURL') {
+          if (dataValue != undefined && dataValue != null) {
+            dataValue = '<a href="'+dataValue+'"><img src="images/github-logo.svg" style="width: 40px; height: 40px" alt="Go to branch" /></a>';
+          } else {
+            dataValue = 'N/A';
+          }
+
+        }
+
+        td.html(dataValue);
+
+        row.append(td);
+      });
     });
   });
-});
+}
+setInterval(updateInspectors, 30000)
+updateInspectors();
